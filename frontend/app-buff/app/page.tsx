f2f393b90f502fafbb2e3b4df5bc11ee31d1de8a
HM9901 <99601204+HM9901@users.noreply.github.com>
2025-04-11 14:08:59 +0700

    Merge branch 'client' into dev

--- frontend/app-buff/app/page.tsx
+++ frontend/app-buff/app/page.tsx
@@ -1,10 +1,16 @@
 
 import Tiktok from "@/components/TiktokSidebar"
-// import RouterPage from '@/router';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <main>
+
+      <Head>
+        <title>HCM57 App BUFF TikTok</title>
+        <meta name="description" content="App buff tiktok" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       {/* <Hero/> */}
       {/* <RouterPage/> */}
       <Tiktok />
